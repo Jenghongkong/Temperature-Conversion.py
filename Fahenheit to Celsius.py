b635175daa114dd90ca215de3a287216e2261e3f
Jenghongkong <jenghongkong@gmail.com>
2020-11-28 15:48:33 +0800

    Update Fahenheit to Celsius.py

--- Fahenheit to Celsius.py
+++ Fahenheit to Celsius.py
@@ -1,5 +1,4 @@
-import math
-x = float(input("Please enter a value for x: "))
-n = int(input("Please enter a value for n: "))
-ans = x**n
-print("The 8th root of %d is %d" %x %ans)
+while True:
+    f = float(input("Give the temperature in Fahrenheit? "))
+    c = ((f - 32) *5 /9)
+    print(c)
